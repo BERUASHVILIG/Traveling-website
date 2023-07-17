22b500437f99233c9c80e965942679c9e68bfa71
giorgi beruashvili <giorgiberuashvili112@gmail.com>
2023-07-17 18:02:27 +0400

    googelSignIn firebase

--- src/components/Destination/DestinationData/DestinationData.tsx
+++ src/components/Destination/DestinationData/DestinationData.tsx
@@ -1,11 +1,15 @@
+import Aos from "aos";
 import "../DestiantionStyles.css";
 
-import React from "react";
+import React, { useEffect } from "react";
 
 const DestinationData = (props: any) => {
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
   return (
     <div>
-      <div className="destination">
+      <div className="destination" data-aos="zoom-in-up">
         <div className={props.className}>
           <div className="des-text">
             <h2>{props.heading}</h2>
--- src/pages/auth/SignUp.tsx
+++ src/pages/auth/SignUp.tsx
@@ -1,79 +1,83 @@
-// import React from "react";
-// import Hero from "../../components/Hero";
-// import SignImg from "../../assets/12.jpg";
-
-// const SignUp = () => {
-//   return (
-//     <div>
-//       <Hero cName="hero-mid" heroImg={SignImg} title="SignUp" />
-//       <div className="hero-mid">
-//         <div className="form-container">
-//           <form>
-//             <input type="text" placeholder="Email" />
-//             <input type="text" placeholder="Password" />
-//             <button>Sign Up</button>
-//           </form>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default SignUp;
-
-import React, { useState } from "react";
-import { auth } from "../../firebase/Firebase";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import React, { useEffect, useState } from "react";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
 import Hero from "../../components/Hero";
-
 import SignImg from "../../assets/12.jpg";
 import { Link } from "react-router-dom";
+import Aos from "aos";
 
 const SignUp = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const signUp = (e: any) => {
+  const [user, setUser] = useState<any>();
+
+  const signUpWithEmailPassword = (e: any) => {
     e.preventDefault();
-    createUserWithEmailAndPassword(auth, email, password)
+    createUserWithEmailAndPassword(getAuth(), email, password)
       .then((userCredential) => {
         setEmail("");
         setPassword("");
+        alert("Successfully signed up with email and password!");
         alert(userCredential);
       })
       .catch((error) => {
-        alert(error);
+        alert(error.message);
       });
   };
+
+  const signUpWithGoogle = () => {
+    const provider = new GoogleAuthProvider();
+    const auth = getAuth();
+
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log(result.user);
+        console.log(result.user.email);
+        alert("Successfully signed up with Google!");
+        setUser(result.user.email);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
+
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
+
   return (
     <div>
-      <Hero cName="hero-mid" heroImg={SignImg} title="SignUp" />
-      {isAuth ? (
-        <p>Profile</p>
-      ) : (
-        <div className="hero-mid">
-          <div className="form-container">
-            <h1>Sign Up</h1>
-            <p>Create Account</p>
-            <form onSubmit={signUp}>
-              <input
-                type="text"
-                placeholder="Email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <input
-                type="Password"
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <button type="submit">Sign Up</button>
-              <Link to={"/signin"}>SignIn</Link>
-            </form>
-          </div>
+      <div className="hero-mid" data-aos="zoom-in-left">
+        <div className="form-container">
+          <h1>Sign Up</h1>
+          <p>Create Account</p>
+          <form onSubmit={signUpWithEmailPassword}>
+            <input
+              type="text"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button type="submit">Sign Up with Email and Password</button>
+          </form>
+          <button onClick={signUpWithGoogle}>Sign Up with Google</button>
+          <Link to={"/signin"}>SignIn</Link>
         </div>
-      )}
+        <div>{user}</div>
+      </div>
     </div>
   );
 };
